1c115523e5bef3310425667a7539cf5b8fc60d44
danielpaiva1993 <40474508+danielpaiva1993@users.noreply.github.com>
2018-06-24 15:30:14 -0300

    Add files via upload

--- TesteTrab.c
+++ TesteTrab.c
@@ -8,12 +8,48 @@ typedef struct arvm{
 	int nchaves, folha;
 	char **chaves;
 	//===================
-	char *nomedisco;
+	char *album;
 	int nfaixas;
 	int minutos;
 	//===================
 	struct arvm **filho, *antirmao, *proxirmao;
-}TABM;
+}TDISCO;
+
+
+TDISCO* busca(TDISCO* a, char *chave){
+	if(!a) return a;
+
+	int i = 0;
+
+	while((i < a->nchaves) && (a->chaves[i] < a)) i++;
+
+	if((i < a->nchaves) && (a->folha) && (chave == a->chaves[i])) return a;
+
+	return busca(a->filho[i], chave);
+}
+
+char *busca_album(TDISCO* a, char *chave){
+	TDISCO* achado = busca(a, chave);
+	if(!achado) return achado;
+
+	return achado->album;
+}
+
+int busca_faixas(TDISCO* a, char *chave){
+	TDISCO* achado = busca(a, chave);
+	if(!achado) return achado;
+
+	return achado->nfaixas;
+}
+
+int busca_minutos(TDISCO* a, char *chave){
+	TDISCO* achado = busca(a, chave);
+	if(!achado) return achado;
+
+	return achado->minutos;
+}
+
+
 
 int main(){
 	char *um,*dois;
