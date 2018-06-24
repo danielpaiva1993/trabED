d9e73eddcede73cf9f1c361be53e5056536ab660
danielpaiva1993 <40474508+danielpaiva1993@users.noreply.github.com>
2018-06-24 10:37:33 -0300

    Add files via upload
    Fiz a busca, não coloquei o t como parâmetro pq ele já foi definido no início do programa

--- TesteTrab.c
+++ TesteTrab.c
@@ -13,7 +13,24 @@ typedef struct arvm{
 	int minutos;
 	//===================
 	struct arvm **filho, *antirmao, *proxirmao;
-}TABM;
+}TDISCO;
+
+
+TDISCO *busca(TDISCO* a, char *chave){
+	if(!a) return a;
+
+	int i = 0;
+
+	while((i < a->nchaves) && (a->chaves[i] < a)) i++;
+
+	if((i < a->nchaves) && (a->folha) && (chave == a->chaves[i])) return a;
+
+	return busca(a->filho[i], chave);
+}
+
+
+
+
 
 int main(){
 	char *um,*dois;
